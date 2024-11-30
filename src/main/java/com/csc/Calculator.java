4b51674d2d674edcd7f6cd7ddd0c9ddc85b1cf45
vs25965 <vs25965@mail.harpercollege.edu>
2024-11-30 14:20:30 -0600

    Lab

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,7 +1,107 @@
 package com.csc;
 
+import java.util.Arrays;
+
 public class Calculator {
-  public static void main(String[] args) {
-    System.out.println("This class can house your driver program and / or your functions.");
-  }
+  
+  public int maximumUsingForLoop (int[] nums) 
+    {
+      if (nums.length == 0)
+      {
+        return -1;
+      }
+      else{
+        int max = nums[0];
+       for( int i = 1; i < (nums.length); i++ )
+       {
+         if (nums[i] > max)
+         {
+           max = nums[i];
+         }
+       }
+       return max;
+
+      }
+      
+    }
+    public int minimumUsingForLoop(int[] nums)
+    {
+      if (nums.length == 0)
+      {
+        return -1;
+      }
+      else{
+        int min = nums[0];
+       for( int i = 1; i < (nums.length); i++ )
+       {
+         if (nums[i] < min)
+         {
+           min = nums[i];
+         }
+       }
+       return min;
+
+      }
+    }
+    public int sumUsingForLoop(int[] nums)
+    {
+      if (nums.length == 0)
+      {
+        return -1;
+      }
+      else
+      {
+        int sum = nums[0];
+        for( int i = 1; i < (nums.length); i++ )
+        {
+          sum += nums[i];
+        }
+        return sum;
+      }
+    }
+    public  int averageUsingForLoop(int[] nums)
+    {
+      if (nums.length == 0)
+      {
+        return -1;
+      }
+      else
+      {
+      int ave = (sumUsingForLoop(nums) / nums.length) ;
+      return ave;
+      }
+    }
+    public  int minimumUsingStream(int[] nums) 
+    { 
+      int min;
+      min = Arrays.stream(nums).min().orElseThrow(() -> new IllegalArgumentException("Array is empty"));
+      return min;
+      
+    }
+    public int maximumUsingStream(int[] nums) 
+    {  
+      int max;
+      max = Arrays.stream(nums).max().orElseThrow(() -> new IllegalArgumentException("Array is empty"));
+      return max;
+    }
+    public int sumUsingStream(int[] nums)
+    { 
+      if (nums.length == 0)
+      {
+        return -1;
+      }
+      else
+      {
+      int sum = Arrays.stream(nums).sum(); // does not use throwException b/c int contrast to the other streams it has a meaningful default value(0)
+      return sum;
+      }
+    }
+    public int averageUsingStream(int[] nums) 
+    {
+      double ave_s = Arrays.stream(nums).average().orElseThrow(() -> new IllegalArgumentException("Array is empty"));
+      int ave =(int) ave_s; //truncate double to return int
+      return ave;
+    }
+     
+  
 }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 public class TestCalculator {
 
   Calculator calculator;
+  int[] _try = {1,11,5,0}; 
+  int[] emptySet = {}; 
 
   @BeforeEach
   void setUp() {
@@ -15,7 +17,59 @@ public class TestCalculator {
   }
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void minimumUsingForLoopTest() {
+    int min_ = calculator.minimumUsingForLoop(_try );
+   assertEquals(0,min_ );
   }
+  @Test
+  void maximumUsingForLoopTest() {
+   assertEquals(11,calculator.maximumUsingForLoop(_try));
+  }
+  @Test
+  void sumUsingForLoopTest() {
+   assertEquals(17,calculator.sumUsingForLoop(_try));
+  }
+  @Test
+  void averageUsingForLoopTest() {
+   assertEquals(4,calculator.averageUsingForLoop(_try));
+  }
+  @Test
+  void minimumUsingStreamTest() {
+    int min_ = calculator.minimumUsingStream(_try );
+   assertEquals(0,min_ );
+  }
+  @Test
+  void maximumUsingStream() {
+   assertEquals(11,calculator.maximumUsingStream(_try));
+  }
+  @Test
+  void sumUsingStreamTest() {
+   assertEquals(17,calculator.sumUsingStream(_try));
+  }
+  @Test
+  void averageUsingStreamTest() {
+   assertEquals(4,calculator.averageUsingStream(_try));
+  }
+
+  //Test empty set 
+  @Test
+  void minimumUsingForLoopTest2() {
+    int min_ = calculator.minimumUsingForLoop(emptySet);
+   assertEquals(-1,min_ );
+  }
+  @Test
+  void sumUsingForLoopTest2() {
+   assertEquals(-1,calculator.sumUsingForLoop(emptySet));
+  }
+  @Test
+  void averageUsingForLoopTest2() {
+   assertEquals(-1,calculator.averageUsingForLoop(emptySet));
+  }
+
+  @Test
+  void sumUsingStreamTest2() {
+   assertEquals(-1,calculator.sumUsingStream(emptySet));
+  }
+ 
 }
+
